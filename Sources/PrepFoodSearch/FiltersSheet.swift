d1e08a3a5681750d02c64a39a4f853fc9fc7db41
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-24 21:00:15 +0500

    bump

--- Sources/PrepFoodSearch/FiltersSheet.swift
+++ Sources/PrepFoodSearch/FiltersSheet.swift
@@ -2,11 +2,12 @@ import SwiftUI
 import SwiftUISugar
 import SwiftHaptics
 
-struct DatabaseButton: View {
+struct FilterButton: View {
     
     @Environment(\.colorScheme) var colorScheme
     
-    @Binding var database: Database
+    @Binding var filter: Filter
+    @Binding var onlyOneFilterSelectedInGroup: Bool
     let didTap: () -> ()
     
     var body: some View {
@@ -14,17 +15,37 @@ struct DatabaseButton: View {
 //        button
     }
     
+    var isOnlySelectedFilterInGroup: Bool {
+        onlyOneFilterSelectedInGroup && filter.isSelected
+    }
+    
     var toggle: some View {
-        Toggle(isOn: $database.isSelected) {
+        let isOn = Binding<Bool>(
+            get: {
+                /// Always show as toggled if this is the only selected filter in the group
+                guard !isOnlySelectedFilterInGroup else {
+                    return true
+                }
+                return filter.isSelected
+            },
+            set: {
+                guard !isOnlySelectedFilterInGroup else {
+                    return
+                }
+                filter.isSelected = $0
+            }
+        )
+
+        return Toggle(isOn: isOn) {
             HStack {
                 optionalImage
-                Text(database.name)
+                Text(filter.name)
             }
             .frame(height: 25)
         }
         .toggleStyle(.button)
         .buttonStyle(.bordered)
-        .tint(database.isSelected ? .accentColor : .gray)
+        .tint(filter.isSelected ? .accentColor : .gray)
     }
     
     @ViewBuilder
@@ -39,7 +60,7 @@ struct DatabaseButton: View {
     var button: some View {
         Button {
             withAnimation {
-                database.toggle()
+                filter.toggle()
             }
             didTap()
         } label: {
@@ -48,8 +69,8 @@ struct DatabaseButton: View {
                     .foregroundColor(backgroundColor(for: colorScheme))
                 HStack(spacing: 5) {
                     optionalImage
-                    Text(database.name)
-                        .foregroundColor(database.isSelected ? .white : .primary)
+                    Text(filter.name)
+                        .foregroundColor(filter.isSelected ? .white : .primary)
                 }
                 .padding(.horizontal, 15)
                 .padding(.vertical, 8)
@@ -60,19 +81,18 @@ struct DatabaseButton: View {
 //        .grayscale(filter.isSelected ? 1 : 0)
     }
     
-    var systemImageColor: Color {
-        if let selectedColor = database.selectedSystemImageColor, database.isSelected {
-            return selectedColor
-        } else {
-            return .secondary
+    var systemImageColor: Color? {
+        guard let selectedColor = filter.selectedSystemImageColor, filter.isSelected else {
+            return nil
         }
+        return selectedColor
     }
     
     var systemImage: String? {
-        if database.isSelected, let selectedSystemImage = database.selectedSystemImage {
+        if filter.isSelected, let selectedSystemImage = filter.selectedSystemImage {
             return selectedSystemImage
         } else {
-            return database.systemImage
+            return filter.systemImage
         }
     }
     
@@ -80,24 +100,160 @@ struct DatabaseButton: View {
         let selectionColorDark = Color(hex: "6c6c6c")
         let selectionColorLight = Color(hex: "959596")
         
-        guard database.isSelected else {
+        guard filter.isSelected else {
             return Color(.secondarySystemFill)
         }
         return colorScheme == .light ? selectionColorLight : selectionColorDark
     }
 }
 
+extension Array where Element == Filter {
+    var selectedCount: Int {
+        filter({ $0.isSelected }).count
+    }
+}
+class FiltersSheetViewModel: ObservableObject {
+    @Published var databaseFilters: [Filter] = allDatabases {
+        didSet {
+            withAnimation {
+                onlyOneDatabaseIsSelected = databaseFilters.selectedCount == 1
+                footerText = getFooterText()
+            }
+        }
+    }
+    @Published var typeFilters: [Filter] = allTypes {
+        didSet {
+            withAnimation {
+                onlyOneTypeIsSelected = typeFilters.selectedCount == 1
+                footerText = getFooterText()
+            }
+        }
+    }
+    @Published var onlyOneDatabaseIsSelected: Bool
+    @Published var onlyOneTypeIsSelected: Bool
+    @Published var footerText: Text
+    
+    init() {
+        //TODO: Read in and set defaults here
+        self.databaseFilters = allDatabases
+        self.typeFilters = allTypes
+        self.onlyOneDatabaseIsSelected = allDatabases.selectedCount == 1
+        self.onlyOneTypeIsSelected = allTypes.selectedCount == 1
+        self.footerText = Text("")
+    }
+    
+    var foodsCount: Int {
+        //TODO: Get this from the backend
+        /// We should store these values in UserDefaults.
+        /// Update it silently in the background whenever the app or this sheet is opened.
+        let verifiedFoods = 13543
+        let verifiedRecipes = 523
+        let verifiedPlates = 93
+        let myFoods = 154
+        let myRecipes = 12
+        let myPlates = 4
+        let usdaFoods = 381939
+        let usdaRecipes = 0
+        let usdaPlates = 0
+        let ausnutFoods = 5740
+        let ausnutRecipes = 0
+        let ausnutPlates = 0
+        
+        var count = 0
+        if verifiedSelected {
+            if foodsSelected { count += verifiedFoods }
+            if recipesSelected { count += verifiedRecipes }
+            if platesSelected { count += verifiedPlates }
+        }
+        if yourDatabaseSelected {
+            if foodsSelected { count += myFoods }
+            if recipesSelected { count += myRecipes }
+            if platesSelected { count += myPlates }
+        }
+        if usdaSelected {
+            if foodsSelected { count += usdaFoods }
+            if recipesSelected { count += usdaRecipes }
+            if platesSelected { count += usdaPlates }
+        }
+        if ausnutSelected {
+            if foodsSelected { count += ausnutFoods }
+            if recipesSelected { count += ausnutRecipes }
+            if platesSelected { count += ausnutPlates }
+        }
+        return count
+    }
+    
+    var typesSuffix: String {
+        if foodsSelected {
+            if recipesSelected {
+                if platesSelected {
+                    return "foods, recipes and plates"
+                } else {
+                    return "foods and recipes"
+                }
+            } else if platesSelected {
+                return "foods and plates"
+            } else {
+                return "foods"
+            }
+        } else {
+            if recipesSelected {
+                if platesSelected {
+                    return "recipes and plates"
+                } else {
+                    return "recipes"
+                }
+            } else {
+                return "plates"
+            }
+        }
+    }
+
+    func getFooterText() -> Text {
+        Text("You are searching \(foodsCount) \(typesSuffix).")
+    }
+    
+    var verifiedSelected: Bool {
+        databaseFilters.first(where: { $0.name == "Verified" })?.isSelected == true
+    }
+    var yourDatabaseSelected: Bool {
+        databaseFilters.first(where: { $0.name == "Your Database" })?.isSelected == true
+    }
+    var usdaSelected: Bool {
+        databaseFilters.first(where: { $0.name == "USDA" })?.isSelected == true
+    }
+    var ausnutSelected: Bool {
+        databaseFilters.first(where: { $0.name == "AUSNUT" })?.isSelected == true
+    }
+
+    var foodsSelected: Bool {
+        typeFilters.first(where: { $0.name == "Foods" })?.isSelected == true
+    }
+    var platesSelected: Bool {
+        typeFilters.first(where: { $0.name == "Plates" })?.isSelected == true
+    }
+    var recipesSelected: Bool {
+        typeFilters.first(where: { $0.name == "Recipes" })?.isSelected == true
+    }
+}
+
 struct FiltersSheet: View {
     
-    @State var databases: [Database] = allDatabases
+    @StateObject var viewModel = FiltersSheetViewModel()
+
+    init() { }
     
     var body: some View {
         NavigationView {
             FormStyledScrollView {
-                FormStyledSection(header: header, footer: footer) {
+                FormStyledSection(header: Text("Databases")) {
                     databasesSection
                 }
+                FormStyledSection(header: Text("Types"), footer: footer) {
+                    typesSection
+                }
             }
+            .onAppear(perform: appeared)
             .navigationTitle("Search Settings")
             .navigationBarTitleDisplayMode(.inline)
         }
@@ -105,57 +261,93 @@ struct FiltersSheet: View {
         .presentationDragIndicator(.hidden)
     }
     
-    var header: some View {
-        Text("Databases")
-    }
-    
-    var foodsCount: Int {
-        databases.reduce(0) { $0 + ($1.isSelected ? $1.count : 0) }
+    func appeared() {
+        viewModel.footerText = viewModel.getFooterText()
     }
     
     var footer: some View {
-        Text("You are searching through \(foodsCount) foods.")
+        viewModel.footerText
     }
     
     var databasesSection: some View {
         FlowLayout(
             mode: .scrollable,
-            items: databases,
+            items: viewModel.databaseFilters,
             itemSpacing: 4,
             shouldAnimateHeight: .constant(false)
         ) {
             databaseButton(for: $0)
         }
     }
-    
+
+    var typesSection: some View {
+        FlowLayout(
+            mode: .scrollable,
+            items: viewModel.typeFilters,
+            itemSpacing: 4,
+            shouldAnimateHeight: .constant(false)
+        ) {
+            typeButton(for: $0)
+        }
+    }
+
     @ViewBuilder
-    func databaseButton(for database: Database) -> some View {
-        if let index = databases.firstIndex(where: { $0.id == database.id }) {
-            DatabaseButton(database: $databases[index]) {
+    func databaseButton(for database: Filter) -> some View {
+        if let index = viewModel.databaseFilters.firstIndex(where: { $0.id == database.id }) {
+            FilterButton(
+                filter: $viewModel.databaseFilters[index],
+                onlyOneFilterSelectedInGroup: $viewModel.onlyOneDatabaseIsSelected) {
             }
             .buttonStyle(.borderless)
         }
     }
+    
+    @ViewBuilder
+    func typeButton(for database: Filter) -> some View {
+        if let index = viewModel.typeFilters.firstIndex(where: { $0.id == database.id }) {
+            FilterButton(
+                filter: $viewModel.typeFilters[index],
+                onlyOneFilterSelectedInGroup: $viewModel.onlyOneTypeIsSelected) {
+            }
+            .buttonStyle(.borderless)
+        }
+    }
+
 }
 
-var allDatabases: [Database] = [
-    Database(name: "My Foods", count: 768, isSelected: true),
-    Database(
-        name: "Public Verified",
-        count: 12257,
+var allDatabases: [Filter] = [
+    Filter(
+        name: "Verified",
         systemImage: "checkmark.seal",
         selectedSystemImage: "checkmark.seal.fill",
         selectedSystemImageColor: .blue,
         isSelected: true
     ),
-    Database(name: "USDA", count: 381939),
-    Database(name: "AUSNUT", count: 5740)
+    Filter(name: "Your Database", isSelected: true),
+    Filter(name: "USDA"),
+    Filter(name: "AUSNUT")
 ]
 
-struct Database: Identifiable {
+var allTypes: [Filter] = [
+    Filter(
+        name: "Foods",
+        systemImage: "carrot",
+        selectedSystemImage: "carrot.fill",
+        isSelected: true
+    ),
+    Filter(
+        name: "Recipes",
+        systemImage: "note.text"
+    ),
+    Filter(
+        name: "Plates",
+        systemImage: "fork.knife"
+    )
+]
+
+struct Filter: Identifiable {
     var id = UUID().uuidString
     var name: String
-    var count: Int
     var systemImage: String? = nil
     var selectedSystemImage: String? = nil
     var selectedSystemImageColor: Color? = nil
