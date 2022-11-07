027e3d5d61e0b2896ce557085216815ea48b1719
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-07 13:04:17 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pxlshpr/FoodLabel", from: "0.0.29"),
-        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.110"),
+        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.111"),
         .package(url: "https://github.com/pxlshpr/PrepNetworkController", from: "0.0.22"),
         .package(url: "https://github.com/pxlshpr/PrepViews", from: "0.0.23"),
         .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.86"),
